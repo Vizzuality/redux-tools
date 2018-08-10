41ff29da358ab49a9e30205dc5d16a254a627602
j8seangel <jseangel@gmail.com>
2018-08-10 15:37:38 +0200

    Allow import only reducers without *

--- src/handleModule.js
+++ src/handleModule.js
@@ -2,9 +2,14 @@ import handleActions from './handleActions';
 
 export default function handleModule(moduleFile) {
   const { reducers, initialState } = moduleFile;
-  if (!reducers.default) {
-    return console
-      .warn('You are attempting to connect a module that doesn\'t export any default reducers.');
+  let missingPart = '';
+  if (!initialState) missingPart = 'initial state';
+  if (reducers && !reducers.default) {
+    missingPart += missingPart ? 'nor ' : '';
+    missingPart += 'default reducers';
+  }
+  if (missingPart) {
+    console.warn(`You are attempting to connect a module that doesn export any ${missingPart}.`);
   }
   return handleActions(reducers.default || reducers, initialState || {});
 }
