8da36a85231fff0802f879231689a5199cb8e482
Ed Brett <e.j.a.brett@gmail.com>
2018-09-10 16:16:34 +0200

    remove dependancy on default on reducers

--- src/handleModule.js
+++ src/handleModule.js
@@ -4,12 +4,12 @@ export default function handleModule(moduleFile) {
   const { reducers, initialState } = moduleFile;
   let missingPart = '';
   if (!initialState) missingPart = 'initial state';
-  if (reducers && !reducers.default) {
+  if (!reducers) {
     missingPart += missingPart ? 'nor ' : '';
     missingPart += 'default reducers';
   }
   if (missingPart) {
-    console.warn(`You are attempting to connect a module that doesn export any ${missingPart}.`);
+    console.warn(`You are attempting to connect a module that doesn't export any ${missingPart}.`);
   }
-  return handleActions(reducers.default || reducers, initialState || {});
+  return handleActions(reducers, initialState || {});
 }
